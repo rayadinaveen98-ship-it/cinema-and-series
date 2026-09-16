bbca90216b990aa0c838d5578ea9c3ea004087cd
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-16 22:56:26 +0530

    refactor: route dedicated media screens with hash navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import App from "./App";
-import SeriesShelf from "./SeriesShelf";
-import "./styles.css";
-import "./artwork.css";
-import "./detail.css";
-import "./series.css";
-import "./stream-cleanup.css";
+import "./app-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-      <SeriesShelf />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
