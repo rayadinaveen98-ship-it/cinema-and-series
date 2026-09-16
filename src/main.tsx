e1d4816da5f0b201fa9315aa02c5e1d6c882aca0
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-16 22:03:10 +0530

    feat: mount Series Engine shelf on web and mobile

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
+import SeriesShelf from "./SeriesShelf";
 import "./styles.css";
 import "./artwork.css";
 import "./detail.css";
+import "./series.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SeriesShelf />
     </BrowserRouter>
   </StrictMode>,
 );
