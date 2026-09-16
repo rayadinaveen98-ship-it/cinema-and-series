0fed503cf52b61f2bb9cd75b627433753fa0942e
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-16 20:28:38 +0530

    style: load artwork presentation layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./styles.css";
+import "./artwork.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
